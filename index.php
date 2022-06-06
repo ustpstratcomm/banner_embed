ea534b9b8897f066c90b7f77cd256132f4ae5d84
Ericke Gallardo <99780588+AugChemReact@users.noreply.github.com>
2022-06-06 10:24:01 +0800

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Banner</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.js"></script>
+</head>
+<body>
+<video width="100%" autoplay="" loop="" muted="true" playsinline="" src="banner.mp4">
+</body>
+</html>
